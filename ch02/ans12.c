bf0e1f0a612f9e068312678fe8d8c39943817334
潘浩贤 <carson.pan@herely.us>
2018-02-27 15:50:22 +0800

    feat: 最小正子序列和

--- ch02/ans12.c
+++ ch02/ans12.c
@@ -62,9 +62,71 @@ int MinSubsequenceSum_linear(const int A[], int N)
     return MinSum;
 }
 
-int MinSubsequencePositiveSum()
+
+struct Item
 {
-    return 0;
+    int value;
+    int rank;
+};
+
+int MinSubsequencePositiveSum(const int A[], int N)
+{
+    /*
+    思路：
+    0、求出以A[0]为开头的所有子序列和，这个和可以通过相减获得所有子序列和
+    1、以i为数组下标，求出一个S(i)数组，里面是数组的前i项和
+    2、使用一个结构体(S, i)
+    */
+
+    struct Item S[N+1];
+    struct Item tempItem;
+    int TempSum, i, j, MinPosSum;
+
+    TempSum = 0;
+    S[0].value = S[0].rank = 0;
+    for(i=0; i<N; i++)
+    {
+        TempSum += A[i];
+        S[i+1].value = TempSum;
+        S[i+1].rank = i + 1;
+    }
+
+    // 插入排序
+    for(i=1; i<N+1; i++)
+    {
+        tempItem.value = S[i].value;
+        tempItem.rank = S[i].rank;
+        j = i - 1;
+        while(j >= 0)
+        {
+            if(S[j].value > S[j+1].value)
+            {
+                S[j+1].value = S[j].value; S[j+1].rank = S[j].rank;
+                S[j].value = tempItem.value; S[j].rank = tempItem.rank;
+            }
+            j -= 1;
+        }
+    }
+
+    // for(i=0; i<N+1; i++)
+    //     printf("%d ", S[i].value);
+
+    MinPosSum = 0;
+    for(i=0; i<N+1-1; i++)
+    {
+        if(S[i+1].rank > S[i].rank)
+        {
+            if((S[i+1].value - S[i].value) < MinPosSum && (S[i+1].value - S[i].value) > 0)
+            {
+                MinPosSum = (S[i+1].value - S[i].value);
+            }
+            else if(MinPosSum <= 0)
+                MinPosSum = (S[i+1].value - S[i].value);
+            // printf("fuxk rank: %d - %d and MInPosSum = %d, %d\n", S[i+1].rank, S[i].rank, MinPosSum, (S[i+1].value - S[i].value));
+        }
+    }
+
+    return MinPosSum;
 }
 
 int MaxSubsequenceMuti()
@@ -75,12 +137,15 @@ int MaxSubsequenceMuti()
 int main(int argc, char *argv[])
 {
     int a[] = {4, -7, 5, -2, -4, 2, 6, -2};
-    int result;  // -8
+    int result;
     result = MinSubsequenceSum(a, 0, 7);
-    printf("exec MinSubsequenceSum have result = %d\n\n", result);
+    printf("exec MinSubsequenceSum have result = %d\n\n", result);  /* =-8 */
 
     result = MinSubsequenceSum_linear(a, 8);
-    printf("exec MinSubsequenceSum_linear have result = %d\n\n", result);
+    printf("exec MinSubsequenceSum_linear have result = %d\n\n", result);  /* =-8 */
+
+    result = MinSubsequencePositiveSum(a, 8);
+    printf("exec MinSubsequencePositiveSum have result = %d\n\n", result);  /* =1 */
 
     return 0;
 }
